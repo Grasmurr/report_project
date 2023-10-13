67883bc035c4514a12a4703440c115661e0d4458
Суханицкий Роман <suhanickiy.roman@wb.ru>
2023-10-13 11:20:59 +0300

    Добавил несколько взаимодействий

--- service/telegram_bot/handlers/main_menu.py
+++ service/telegram_bot/handlers/main_menu.py
@@ -27,6 +27,7 @@ class AdminStates(StatesGroup):
     main = State()
     manage_events = State()
     enter_event_name = State()
+    delete_event = State()
     confirm_event_name = State()
     enter_count_of_event_prime = State()
     enter_count_of_event_normal = State()
@@ -46,15 +47,28 @@ async def admin_menu(message: Message, state: FSMContext):
 @dp.message(AdminStates.main, F.text == 'Управление мероприятиями')
 async def manage_events(message: Message, state: FSMContext):
     markup = chat_backends.create_keyboard_buttons('Создать мероприятие', 'Удалить мероприятие', 'Назад')
+    await state.set_state(AdminStates.manage_events)
     await message.answer(text='Что вы хотите сделать?',
                          reply_markup=markup)
 
 
-@dp.message(AdminStates.main, F.text == 'Создать мероприятие')
+@dp.message(AdminStates.manage_events, F.text == 'Создать мероприятие')
 async def create_event(message: Message, state: FSMContext):
     await message.answer(text='Введите название мероприятия',
                          reply_markup=ReplyKeyboardRemove())
     await state.set_state(AdminStates.enter_event_name)
+
+
+@dp.message(AdminStates.manage_events, F.text == 'Удалить мероприятие')
+async def delete_event(message: Message, state: FSMContext):
+    await message.answer(text='Выберите мероприятие, которое вы хотите удалить:',
+                         reply_markup=ReplyKeyboardRemove())
+    await state.set_state(AdminStates.delete_event)
+
+
+@dp.message(AdminStates.manage_events, F.text == 'Назад')
+async def back(message: Message, state: FSMContext):
+    await admin_menu(message, state)
 
 
 @dp.message(AdminStates.enter_event_name)
@@ -70,7 +84,7 @@ async def enter_event_name(message: Message, state: FSMContext):
 @dp.message(AdminStates.confirm_event_name)
 async def question_continue_create_event(message: Message, state: FSMContext):
     if message.text == 'Назад':
-        await admin_menu(message=message)
+        await manage_events(message, state)
     else:
         await message.answer(text='Хорошо. Теперь введите число прайм билетов, которое вы хотите '
                                   'создать для этого мероприятия:',
@@ -88,7 +102,7 @@ async def create_count_of_prime_tickets (message: Message, state: FSMContext):
         await state.update_data(count_of_prime_tickets=count_of_prime_tickets)
         await state.set_state(AdminStates.enter_count_of_event_normal)
     else:
-        await message.answer('Кажется, вы ввели что-то неправильно, попробуйте снова...')
+        await message.answer('Попробуйте ввести количество цифрой. Например: 150')
 
 
 @dp.message(AdminStates.enter_count_of_event_normal)
@@ -105,6 +119,8 @@ async def create_count_of_normal_tickets (message: Message, state: FSMContext):
                              reply_markup=markup)
         await state.update_data(count_of_normal_tickets=count_of_normal_tickets)
         await state.set_state(AdminStates.saving_or_editing_from_the_beginning)
+    else:
+        await message.answer('Попробуйте ввести количество цифрой. Например: 150')
 
 
 @dp.message(AdminStates.saving_or_editing_from_the_beginning)
