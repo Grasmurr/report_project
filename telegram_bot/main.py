875632d9e3bf4a0260ffefe8cc44c6d052898352
Суханицкий Роман <suhanickiy.roman@wb.ru>
2023-10-15 11:46:39 +0300

    finally bugs resolved

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,7 +1,6 @@
 import logging, sys, asyncio
 import aiogram
 from handlers import dp, bot
-from loader import dp, bot
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
