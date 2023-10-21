b41954bd10fe799587b2736351a9e60c194ab689
yulyakonnova <yulyakonnovaof@gmail.com>
2023-10-21 17:31:45 +0300

    добавила  функцию удаления билетов

--- telegram_bot/loader.py
+++ telegram_bot/loader.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from assets.configs.config import token
+# from assets.configs.config import token
 
 
-bot = Bot(token=token)
+bot = Bot(token="6529076927:AAF3IIlMbvk2nDIZvxPpH9Kt0N8c-lbTI3w")
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
