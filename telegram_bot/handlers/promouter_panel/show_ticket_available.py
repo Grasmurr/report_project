a4e6ecfda38e57754bdf6b5252a231d407fb995e
yulyakonnova <yulyakonnovaof@gmail.com>
2023-11-04 21:33:20 +0300

    добавила  функцию удаления билетов

---
+++ telegram_bot/handlers/promouter_panel/show_ticket_available.py
@@ -0,0 +1,8 @@
+from telegram_bot.loader import dp, bot
+from aiogram.types import \
+    (Message,
+     ReplyKeyboardRemove
+     )
+from telegram_bot.helpers import chat_backends
+from aiogram import F
+from aiogram.fsm.context import FSMContext
