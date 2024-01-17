3c3597a5e4d16f06e4781594fef63086ca5748b6
Роман Суханицкий <grasmurr@gmail.com>
2024-01-17 12:57:02 +0300

    fixed some bugs

--- telegram_bot/gdrive/api_methods.py
+++ telegram_bot/gdrive/api_methods.py
@@ -40,13 +40,13 @@ def create_sheet(list_name):
 def format_data_for_google_sheets(data):
     headers = [
         "Мероприятие", "Номер билета", "Имя",
-        "Фамилия", "Тип билета", "Дата рождения",
+        "Фамилия", "Пол", "Тип билета", "Дата рождения",
         "Цена", "Программа обучения", "Курс", "Номер телефона", "Возврат", "Предтавитель", "Дата"
     ]
 
     rows = [[
         item["event"], item["ticket_number"],
-        item["ticket_holder_name"], item["ticket_holder_surname"],
+        item["ticket_holder_name"], item["ticket_holder_surname"], item["ticket_holder_sex"],
         item["ticket_type"], item["date_of_birth"], item["price"],
         item["educational_program"], item["educational_course"], item["phone_number"],
         item["is_refunded"], item['promouter_name'], item['date']
